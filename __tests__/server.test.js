c03e16ea19dd4140a105db3d7a402385522a63a7
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2020-10-11 15:40:01 -0500

    tests for frontend static files being served correctly

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -2,8 +2,40 @@ const app = require('../server');
 const supertest = require('supertest');
 
 
-test('runs without crashing',()=>{
-  expect(()=>{
+test('runs without crashing',async ()=>{
+  await expect(()=>{
     supertest(app);
   }).not.toThrow();
+});
+
+describe('the static files are served correctly', () => {
+  test('index.html', async (done) => {
+    supertest(app)
+      .get('/index.html')
+      .expect('Content-Type', /text\/html/)
+      .expect(200)
+      .then(()=>done());
+  });
+  test('/ returns index.html', async (done) => {
+    supertest(app)
+      .get('/')
+      .expect('Content-Type', /text\/html/)
+      .expect(200)
+      .then(()=>done());
+  });
+  test('script.js', async (done) => {
+    supertest(app)
+      .get('/script.js')
+      .expect('Content-Type', /((application)|(text))\/((javascript)|(ecmascript))/)
+      .expect(200)
+      .then(()=>done());
+  });
+  test('style.css', async (done) => {
+    supertest(app)
+      .get('/style.css')
+      .expect('Content-Type', /text\/css/)
+      .expect(200)
+      .then(()=>done());
+  });
+  
 });
